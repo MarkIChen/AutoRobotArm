3621355ca993786a6a2f1a4f3bf53d7f6f641be1
IChen, Chen <c0919766098@yahoo.com.tw>
2019-04-12 21:58:35 +0800

    With CocoaMQTT

---
+++ AutoRobotArm/JSONProvider.swift
@@ -0,0 +1,14 @@
+//
+//  JSONProvider.swift
+//  AutoRobotArm
+//
+//  Created by Mark on 2019/3/24.
+//  Copyright © 2019 MarkChen. All rights reserved.
+//
+
+import Foundation
+
+struct JsonProvider: Codable{
+    var x: Float?
+    var y: Float?
+}
--- AutoRobotArm/ViewController.swift
+++ AutoRobotArm/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class ViewController: UIViewController {
-
+    var mqtt: CocoaMQTT?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupMQTT()
+        
     }
 
-
+    @IBAction func setPositionPressed(_ sender: Any) {
+        mqtt?.publish("home/test", withString: "testing")
+        
+        
+    }
+    @IBAction func SubscribeButtonPressed(_ sender: Any) {
+        
+        mqtt?.subscribe("home/test")
+    }
+    
+    
+    func setupMQTT(){
+        let clientID = "CocoaMQTT-" + String(ProcessInfo().processIdentifier)
+        mqtt = CocoaMQTT(clientID: clientID, host: "35.233.174.129", port: 1883)
+        mqtt?.autoReconnect = true
+        mqtt?.willMessage = CocoaMQTTWill(topic: "/will", message: "dieout")
+        mqtt?.keepAlive = 60
+        mqtt?.delegate = self as? CocoaMQTTDelegate
+        if let mqttStatus = mqtt?.connect(){
+            print(mqttStatus)
+        }
+        
+        
+        
+        mqtt?.didSubscribeTopic = {
+            mqtt , topic in
+            print("I subscribed the robot")
+        }
+        
+        mqtt?.didReceiveMessage = { mqtt, message, id in
+            print("Message received in topic \(message.topic) with payload \(message.string!)")
+            let messageData = (message.string)?.data(using: .utf8)
+            
+            if let data = messageData , let result = try? JSONDecoder().decode(JsonProvider.self, from: data) {
+                
+             
+                if let x = result.x , let y = result.y{
+                    print("x: \(x)")
+                    print("y: \(y)")
+                }
+            }
+            else {
+                print("Failed to get the message!")
+            }
+        }
+        
+        mqtt?.didPublishMessage = {
+            mq, message, a in
+//            print(message.string)
+        }
+        
+    }
 }
 
